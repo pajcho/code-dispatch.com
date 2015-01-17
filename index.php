a6f977c2368945e4ac33a27671f20cf53a6ec890
Nikola Pajic <nikola.pajic@gmail.com>
2015-01-17 18:23:10 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Code Dispatch - Coming Soon</title>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="transy">
+</div>
+<div id="wrapper">
+    <div class="logo">
+        <h1>Code Dispatch</h1>
+        <h2><strong class="one"></strong>application development<strong class="two"></strong></h2>
+    </div>
+    <div class="content">
+        <p class="big_text">we are working on <strong>making this site happen!</strong></p>
+    </div>
+</div>
+</body>
+</html>
